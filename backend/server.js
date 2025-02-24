8e2b46b97d0d6393ec52f6b6476d9d06fcd01094
ZUBAYER HOSSEN <125750411+zubayer-hossen@users.noreply.github.com>
2025-02-24 07:33:00 +0600

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,9 +6,22 @@ const mongoose = require('mongoose');
 const userRouter = require('./routes/userRoutes');
 const path = require('path');
 
+
+const corsOptions = {
+  origin: 'https://special-economics-care-tour-register.onrender.com',  // Allow only this frontend domain
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow only specific methods
+  allowedHeaders: ['Content-Type', 'Authorization'], // Allow specific headers
+  credentials: true, // Allow cookies and credentials to be included in requests
+  preflightContinue: false,  // Let Express handle preflight requests automatically
+  optionsSuccessStatus: 204  // Status code for successful OPTIONS requests
+};
+
+
+
 // Initialize the app
 const app = express();
-app.use(cors());
+// Apply CORS settings globally
+app.use(cors(corsOptions));
 app.use(express.json());
 
 // Connect to MongoDB using the URI from the environment variables
@@ -27,7 +40,7 @@ app.use('/api/users', userRouter);
 app.use('/pdfs', express.static(path.join(__dirname, 'pdfs')));
 
 // Port
-const PORT = process.env.PORT || 5000;
+const PORT = 'https://tour-user.onrender.com' || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
